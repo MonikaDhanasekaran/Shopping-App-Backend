7c771afe075a08e0419d2dd1e50acad780c4c599
Monika Dhanasekaran <107670734+MonikaDhanasekaran@users.noreply.github.com>
2023-04-18 18:37:20 +0530

    Add files via upload

---
+++ models/Furnitures/swingsModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const swingSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    percent:{
+        type: String,
+        required: true
+    },
+    strikeOutRate:{
+        type: String,
+        required: true
+    },
+    originalRate:{
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("swing", swingSchema);
---
+++ routes/AppliancesRoutes/airConditionersRoutes.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const auth = require("../../models/authModel");
+const ac = require("../../models/Appliances/airConditionersModel");
+const router = express.Router();
+
+router.get("/get", (req,res)=>{
+    try{
+        ac.find((err,data)=>{
+            if(err){
+                return res.status(400).send({ msg: "Error while retrieving data" });
+            }
+            res.status(200).send(data);
+        });
+    }catch(error){
+        res.status(500).send({ msg: "Internal Server Error" });
+    }
+});
+
+router.get("/searchAirConditioners/:key", async(req,res)=>{
+    let result = await ac.find({
+        "$or": [
+            {
+                name: { $regex: req.params.key }
+            },
+            {
+                strikeOutRate: { $regex: req.params.key }
+            },
+            {
+                originalRate: { $regex: req.params.key }
+            },
+        ]
+    });
+    res.send(result);
+});
+
+router.get('/getOne/:dataID', (req, res) => {
+    try{
+        ac.findOne({_id: req.params.dataID}, (err, data) => {
+            if(err){
+                return res.status(400).send({message: 'Error while retrieving an data. Please check the data'})
+            }
+
+            res.status(200).send(data);
+        })
+    }catch(error){
+        res.status(500).send({
+            message: 'Internal Server Error'
+        })
+    }
+});
+
+router.post("/create", auth.authenticateUser, async (req, res) => {
+    try {
+        const payload = req.body;
+        const newData = new ac(payload);
+        await newData.save((err,data) => {
+            if(err){
+                return res.status(400).send({ msg: "Error while adding data" });
+            }
+            res.status(201).send({ dataId: data._id, msg: "Data has been added successfully" });
+        });
+    } catch (err) {
+        res.status(500).send({ msg: "Internal Server Error" });
+        console.log(err);
+    }
+  });
+
+router.put("/update/:dataID", auth.authenticateUser, (req,res)=>{
+    try{
+        ac.findByIdAndUpdate({ _id: req.params.dataID }, { $set: req.body }, (err,data)=>{
+            if(err){
+                return res.status(400).send({ msg: "Error while updating data" });
+            }
+            res.status(201).send({ dataId: data._id, msg: "Data have been updated" });
+        });
+    }catch(error){
+        res.status(500).send({ msg: "Internal Server Error" });
+    }
+});
+
+router.delete("/delete/:dataID",auth.authenticateUser ,(req,res)=>{
+    try{
+        ac.deleteOne({ _id: req.params.dataID }, (err,data)=>{
+            if(err){
+                return res.status(400).send("Error while deleting data");
+            }
+            res.status(200).send({ msg: `Data with id ${req.params.dataID} has been deleted` });
+        });
+    }catch(error){
+        res.status(500).send({ msg: "Internal Server Error" });
+    }
+});
+
+module.exports = router;
